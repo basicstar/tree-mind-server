0c88a26a289245d562e87d25b4d9141308cab96c
basicstar <348607723@qq.com>
2025-04-22 20:08:44 +0800

    更改返回的地址

--- src/app/api/usermarkdown/route.ts
+++ src/app/api/usermarkdown/route.ts
@@ -41,7 +41,9 @@ export async function POST(req: NextRequest) {
     await prisma.usermarkdown.create({
       data: { userid, markdown, crticalID },
     });
-    const linkId = `http://www.apexcode.top/index.html?linkId=${crticalID}`;
+    // 获取完整URL
+    const origin = new URL(req.url).origin;  
+    const linkId = `${origin}/index.html?linkId=${crticalID}`;
     return NextResponse.json({ linkId }, { status: 201 });
   } catch (error) {
     if (error instanceof Error) {
