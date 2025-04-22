0164b54c7518a92bdb8153e75fee523fee0cdce2
basicstar <348607723@qq.com>
2025-04-22 22:50:45 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    basePath: '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? 'https://apexcode.top' : '',
 };
-
 export default nextConfig;
